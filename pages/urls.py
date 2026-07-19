1e2c31a8585b56c263b1a0a0f27b545daf728623
Faizan <gamewithfaixu@gmail.com>
2026-07-19 22:17:34 +0530

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ app_name = 'pages'
 
 urlpatterns = [
     path('about/', views.about_view, name='about'),
+    path('features/', views.features_view, name='features'),
     path('privacy/', views.privacy_view, name='privacy'),
     path('terms/', views.terms_view, name='terms'),
      path('docs/github-integration/', views.github_integration_docs_view, name='github_integration_docs'),
